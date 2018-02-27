96a8a3ada249c65e84968cd2ce1090b30e927bb3
Matt <mattkeegan@btinternet.com>
2018-02-27 14:09:42 +0000

    Update link to mattkeegan.uk

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return(
         <div className="row justify-content-center">
             <div className="col-12 col-md-10 footer">
-                <h5 className='text-center' id='made-by'>Made by <a href='https://codepen.io/mattkeegan20/' target='_blank'>Matt K</a></h5>
+                <h5 className='text-center' id='made-by'>Made by <a href='https://mattkeegan.uk/'>Matt K</a></h5>
             </div>
         </div>
       );
